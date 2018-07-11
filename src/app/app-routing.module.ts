84d06b058ce1a135c13d501e9261b9fcc53f93d1
Unknown <kennyvillanueva15@gmail.com>
2018-07-11 15:35:46 -0500

    check

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 const routes: Routes =[
-  {path: "contact",
-    component: HomeComponent},
-    {path: "", 
-    redirectTo: "/",
-    pathMatch: "Full"
+  {
+    path: "",
+    component: HomeComponent
+  },
+    {
+      path: "", 
+      redirectTo: "/",
+      pathMatch: "full"
       
     }
 ]
