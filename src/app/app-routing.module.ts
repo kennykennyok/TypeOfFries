e4411c087589176d6230ebb9b973188b233c95aa
Marcus <maleach16@gmail.com>
2018-07-11 16:21:38 -0500

    module.ts corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomeComponent } from "./home/home.component";
 import { QuestionComponent } from "./question/question.component";
 import { ResultComponent } from "./result/result.component";
+
 const routes: Routes =[
   {
     path: "",
