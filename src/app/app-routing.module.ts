894d0d9a3c33b49c31d8e500ee6d50e9c6ba7b7e
Unknown <kennyvillanueva15@gmail.com>
2018-07-11 15:51:01 -0500

    routers rigth app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { QuestionComponent } from "./question/question.component";
+import { ResultComponent } from "./result/result.component";
 const routes: Routes =[
   {
     path: "",
     component: HomeComponent
+  },
+  {
+    path: "question/:id",
+    component: QuestionComponent
+  },
+  {
+    path: "result",
+    component: ResultComponent
   },
     {
       path: "", 
