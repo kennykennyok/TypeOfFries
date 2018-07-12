24102b308cc4ca61af0334c7e54db4221abc36bd
Unknown <maleach16@gmail.com>
2018-07-12 11:31:50 -0500

    Changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
 import { HomeComponent } from "./home/home.component";
 import { QuestionComponent } from "./question/question.component";
 import { ResultComponent } from "./result/result.component";
